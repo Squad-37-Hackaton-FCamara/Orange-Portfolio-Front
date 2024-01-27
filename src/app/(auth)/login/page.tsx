0a610c1ce78b7bea3b3a348149ca363db10d73a3
Gabriel <gab.aug.gom@gmail.com>
2024-01-27 16:52:17 -0300

    feat: tela de login e componentes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
-function LoginPage() {
-    return(
-        <div>Login</div>
-    );
-}
+"use client"
+import FormularioLogin from "@/app/_helpers/form";
+import Image from 'next/image'
+import ImgLogin from '../../assets/img_login.png'
 
-export default LoginPage;
+export default function LoginPage() {
+  return (
+    <main className="flex">
+        <Image src={ImgLogin} alt="Ilustração" className="h-[832px] w-[525px] md:hidden"/>
+        <FormularioLogin />
+    </main>
+  );
+}
---
+++ src/app/_helpers/form/index.tsx
@@ -0,0 +1,48 @@
+import TextField from '@mui/material/TextField';
+import CampoSenha from '../campoSenha';
+import Link from 'next/link'
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import clsx from "clsx"
+import { Typography } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF5522',
+      light: '#FF8833',
+      dark: '#CC4400',
+      contrastText: '#FFFFFF',
+    },
+  },
+});
+
+export default function FormularioLogin() {
+  return (
+    <form className='flex flex-col justify-center items-center w-[100vh] h-[100vh]'>
+      <Typography component='h1' className={clsx(
+        'text-5xl mb-5 md:text-2xl',
+        'text-color-principal-90 '
+      )}
+      >Entre no Orange Portfólio</Typography>
+      <div className='flex flex-col justify-between w-[517px] h-[271px] md:w-[312px]'>
+        <Typography component='p' className={clsx(
+          'text-2xl md:text-base',
+          'text-color-neutral-110'
+        )}
+        >Faça login com email</Typography>
+        <TextField
+          id="outlined-basic"
+          label="Email address"
+          variant="outlined"
+          required
+        />
+        <CampoSenha />
+        <ThemeProvider theme={theme}><Button variant="contained" color='primary' className='bg-color-secondary-100'>ENTRAR</Button></ThemeProvider>
+        <Link href="/cadastro" className='text-color-neutral-100 text-base'>Cadastre-se</Link>
+      </div>
+    </form>
+  );
+}
+
