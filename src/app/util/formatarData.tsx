abc8bad210ac097413cc7c9dc5e1d3f441b531b1
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2024-02-02 20:40:40 -0300

    [feat] - Função para formatar data

---
+++ src/app/util/formatarData.tsx
@@ -0,0 +1,9 @@
+export function formatarData(dataString: string): string {
+    console.log(dataString)
+
+    const data = new Date(dataString);
+    const ano = data.getFullYear();
+    const mes = (data.getMonth() + 1).toString().padStart(2, '0'); // Adiciona zero à esquerda se o mês for menor que 10
+    
+    return `${mes}-${ano}`;
+}
