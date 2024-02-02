311965434bee247195a4be825525cfc82548b98d
evanildo <evanildo.sf91@gmail.com>
2024-02-01 22:12:55 -0300

    feat: adicionar "multipart/form-data" as headers

--- src/services/api_projetos.ts
+++ src/services/api_projetos.ts
@@ -1,29 +1,33 @@
-import axios from "axios";
-import 'dotenv/config'
 import { ProjetoProps } from "@/app/@types/Projetos";
+import axios from "axios";
+import "dotenv/config";
 
-export const URL_BASE = "https://nervous-pear-clothes.cyclic.app"
+export const URL_BASE = "https://nervous-pear-clothes.cyclic.app";
 export const HEADERS = (token?: string) => ({
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
+  Accept: "application/json",
+  "Content-Type": "multipart/form-data",
+  Authorization: `Bearer ${token}`,
 });
 
 export class ProjetosAPI {
-static async ListarProjetos({ token }: { token: string }) {
-    const response = await axios.get(
-      `${URL_BASE}/projeto`,
-      { headers: HEADERS(token) }
-    );
-        console.log(URL_BASE)
+  static async ListarProjetos({ token }: { token: string }) {
+    const response = await axios.get(`${URL_BASE}/projeto`, {
+      headers: HEADERS(token),
+    });
+    console.log(URL_BASE);
     return response.data;
   }
 
-static async CriarProjeto({ token, projeto }: { token: string, projeto: ProjetoProps }) {
-    const response = await axios.post(
-      `${URL_BASE}/projeto`,
-      projeto,
-      { headers: HEADERS(token) }
-    );
+  static async CriarProjeto({
+    token,
+    projeto,
+  }: {
+    token: string;
+    projeto: ProjetoProps;
+  }) {
+    const response = await axios.post(`${URL_BASE}/projeto`, projeto, {
+      headers: HEADERS(token),
+    });
 
     return response.data;
   }
