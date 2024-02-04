8390aa8a8c5912395daa8cde1e290b2718897b06
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2024-02-04 14:55:50 -0300

    feat: correcao de bugs no form de add projeto

--- src/app/_helpers/modal/visualizar_projeto/index.tsx
+++ src/app/_helpers/modal/visualizar_projeto/index.tsx
@@ -1,7 +1,27 @@
-// export const ConteudoModalVisualizarProjeto = ({ projeto, setModal, setIsOpen, }) => {
-//     return (
-//         <div>
+import { ProjetoProps } from "@/app/@types/Projetos";
+import { useWindowDimensions } from "@/services/window_size";
+import { Dispatch, SetStateAction } from "react";
 
-//         </div>
-//     )
-// }
+export const ConteudoModalVisualizarProjeto = ({
+  projeto,
+  setModal,
+  setIsOpen,
+}: {
+  projeto: ProjetoProps;
+  setModal: Dispatch<
+    SetStateAction<
+      | ""
+      | "editado"
+      | "adicionado"
+      | "deletado"
+      | "confirmar_deletar"
+      | "editar_projeto"
+      | "add_projeto"
+      | "visualizar_projeto"
+    >
+  >;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const { width } = useWindowDimensions();
+  return width > 768 ? <div></div> : <div>mobile</div>;
+};
